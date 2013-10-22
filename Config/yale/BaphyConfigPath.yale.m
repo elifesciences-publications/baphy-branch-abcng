f33e66573d86268481cfd895fec202053f6eebec
Matthew McGinley <matthew.j.mcginley@gmail.com>
2013-10-22 16:25:44 -0400

    I love baphy

--- Config/yale/BaphyConfigPath.yale.m
+++ Config/yale/BaphyConfigPath.yale.m
@@ -1,5 +1,5 @@
 BAPHY_CONFIG_PATH='lbhb';
-WIN_SERVER_PATH='H:\';
+WIN_SERVER_PATH='C:\Matt\baphy_data\';
 MAC_SERVER_PATH='/Volumes/data/';
 LINUX_SERVER_PATH='/auto/data/';
 DB_SERVER='hearingbrain.org';
