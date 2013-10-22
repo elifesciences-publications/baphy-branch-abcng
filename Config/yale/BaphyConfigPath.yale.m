8ea289199518ef038b1cf2b964dfbf4770674020
Stephen David <davids@ohsu.edu>
2013-10-22 13:05:47 -0700

    Fixed BaphyConfigPath.yale.m to match current net setup.

--- Config/yale/BaphyConfigPath.yale.m
+++ Config/yale/BaphyConfigPath.yale.m
@@ -1,7 +1,7 @@
-BAPHY_CONFIG_PATH='lbhb';
-WIN_SERVER_PATH='H:\';
+BAPHY_CONFIG_PATH='yale';
+WIN_SERVER_PATH='C:\matt\baphy_data\';
 MAC_SERVER_PATH='/Volumes/data/';
 LINUX_SERVER_PATH='/auto/data/';
-DB_SERVER='hearingbrain.org';
-DB_NAME='svd_cell_yale';
-DB_USER='svd_david';
+DB_SERVER='192.168.0.45';
+DB_NAME='cell';
+DB_USER='admin3';
