6413dfc137897d204ddf6839bcdfae905b38a6e7
ABCNG user <abcng.user@gmail.com>
2013-11-29 12:16:47 +0100

    Modified path reading in baphy_set_path

--- Config/abcng/BaphyConfigPath.abcnl.m
+++
@@ -1,7 +0,0 @@
-BAPHY_CONFIG_PATH='abcnl';
-WIN_SERVER_PATH='H:\';
-MAC_SERVER_PATH='/Volumes/data/';
-LINUX_SERVER_PATH='/auto/data/';
-DB_SERVER='129.199.80.89';
-DB_USER='admin';
-DB_PASSWORD='feartheferret';
