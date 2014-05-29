136d061d821dfca23215dd019601405aa96f7bcf
Stephen David <stephen.v.david@gmail.com>
2014-05-29 14:39:50 -0700

    Small fix to @RewardTargetLBHB BehaviorDisplay.m

--- Modules/RefTar/BehaviorObjects/@RewardTargetLBHB/BehaviorDisplay.m
+++ Modules/RefTar/BehaviorObjects/@RewardTargetLBHB/BehaviorDisplay.m
@@ -52,9 +52,9 @@ if ~mod(TrialIndex,exptparams.TrialBlock) || (isempty(AIData) && isempty(TrialSo
         text(0.4,1  ,'DI','FontWeight','bold','HorizontalAlignment','center');
         text(0.5,1  ,'WaR','FontWeight','bold','color','b','HorizontalAlignment','center');
         %text(0.6,1  ,'InfR','FontWeight','bold','HorizontalAlignment','center');
-        text(0.7,1  ,'bHR','FontWeight','bold','color','b','HorizontalAlignment','center');
-        text(0.8,1  ,'bFR','FontWeight','bold','HorizontalAlignment','center');
-        text(0.9,1  ,'bDR','FontWeight','bold','color','b','HorizontalAlignment','center');
+        text(0.6,1  ,'bHR','FontWeight','bold','color','b','HorizontalAlignment','center');
+        text(0.7,1  ,'bFR','FontWeight','bold','HorizontalAlignment','center');
+        text(0.8,1  ,'bDR','FontWeight','bold','color','b','HorizontalAlignment','center');
     end
     % display trialnumber, SR, HR, DR, hitnum, Snooze Hit Rate, Snooze, shams,
     text(0, ind, num2str(TrialIndex),'FontWeight','bold','HorizontalAlignment','center');
@@ -65,9 +65,9 @@ if ~mod(TrialIndex,exptparams.TrialBlock) || (isempty(AIData) && isempty(TrialSo
         text(.4, ind, num2str(exptparams.Performance(end).DiscriminationIndex,'%2.0f'),'FontWeight','bold','HorizontalAlignment','center');
         text(.5, ind, num2str(exptparams.Performance(end).WarningRate,'%2.0f'),'FontWeight','bold','color','b','HorizontalAlignment','center');
         %text(.6, ind, num2str(exptparams.Performance(end).IneffectiveRate,'%2.0f'),'FontWeight','bold','HorizontalAlignment','center');
-        text(.7, ind, num2str(exptparams.Performance(end).RecentHitRate,'%2.0f'),'FontWeight','bold','color','b','HorizontalAlignment','center');
-        text(.8, ind, num2str(exptparams.Performance(end).RecentFalseAlarmRate,'%2.0f'),'FontWeight','bold','HorizontalAlignment','center');
-        text(.9, ind, num2str(exptparams.Performance(end).RecentDiscriminationRate,'%2.0f'),'FontWeight','bold','color','b','HorizontalAlignment','center');
+        text(.6, ind, num2str(exptparams.Performance(end).RecentHitRate,'%2.0f'),'FontWeight','bold','color','b','HorizontalAlignment','center');
+        text(.7, ind, num2str(exptparams.Performance(end).RecentFalseAlarmRate,'%2.0f'),'FontWeight','bold','HorizontalAlignment','center');
+        text(.8, ind, num2str(exptparams.Performance(end).RecentDiscriminationRate,'%2.0f'),'FontWeight','bold','color','b','HorizontalAlignment','center');
         %         text(.14, ind, num2str(exptparams.Performance(end).LickRate,'%2.0f'),'FontWeight','bold','color','b','HorizontalAlignment','center');
         %         text(.14, ind, num2str(exptparams.Performance(end).LickRate,'%2.0f'),'FontWeight','bold','color','b','HorizontalAlignment','center');
         %         text(.14, ind, num2str(exptparams.Performance(end).LickRate,'%2.0f'),'FontWeight','bold','color','b','HorizontalAlignment','center');
