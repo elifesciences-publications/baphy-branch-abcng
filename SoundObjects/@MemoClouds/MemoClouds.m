3fc26a84f5a59d575b6014a1b09db4ff35a214de
ABCNG User <abcng.user@gmail.com>
2015-01-17 20:11:54 +0100

    updated

--- SoundObjects/@MemoClouds/MemoClouds.m
+++ SoundObjects/@MemoClouds/MemoClouds.m
@@ -30,7 +30,7 @@ switch nargin
     o.RateRefRTCPercent      = 25;
     o.SegDur                 = 0.5;
     o.nreps                  = 3;
-        o.ToneCloudDur           = o.SegDur*o.nreps;
+    o.ToneCloudDur           = o.SegDur*o.nreps;
     o.tonespersecond         = 8; % 4, 8, 16, 32, 128, 512
     o.chperoctave            = 4;  % 2, 4,  8, 16,  64, 256
     o.density                = o.tonespersecond*o.chperoctave;
--- SoundObjects/@MemoClouds/gentonecloud22/trandselect.m
+++
@@ -1,4 +0,0 @@
-function xselect=trandselect(x)
-%picks out one of the values of x, with equal probabilities
-
-xselect=x(trand(length(x)));
